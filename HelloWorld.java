877dbe8472612be6dc4c5cf54970e23605dd7b50
colternagel <22colter.nagel@boiseschools.net>
2020-08-20 15:41:05 +0000

    Added comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,14 @@
+//Colter Nagel
+//AP CS A period 2
+//8/20/2020
 
 public class HelloWorld{
 
   public static void main(String args[])
+
+  /* I created a very large I
+  */
+
   {
     System.out.println("||||||||||");
     System.out.println("||||||||||");
@@ -13,10 +20,7 @@ public class HelloWorld{
     System.out.println("   ||||");
     System.out.println("||||||||||");
     System.out.println("||||||||||");
-
   }
 
 
 }
-
-
